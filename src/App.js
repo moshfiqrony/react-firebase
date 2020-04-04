056c25481cf97f599d1d38ee4bbfa6bf6b28ff58
moshfiqrony <moshfiqrony@gmail.com>
2020-04-04 20:41:20 +0600

    MR | New Design Added

--- src/App.js
+++ src/App.js
@@ -38,54 +38,56 @@ function App(props) {
 
 	return (
 		<div className='container'>
-			<div>
-				
-				<div className="row pt-5 justify-content-center d-flex">
-					<div className="col-4 pb-4">
-						<Input
-							name='username'
-							value={username}
-							placeholder='Enter your username'
-							onChange={(event) => setUsername(event.target.value)}
-						/>
-					</div>
-					<div className="col-4">
-						<Input
-							name='username'
-							value={email}
-							placeholder='Enter your email'
-							onChange={(event) => setEmail(event.target.value)}
-						/>
-					</div>
-					<div className="col-4">
-						<Button 
-							type='primary'
-							onClick={handleClick}
-						>Add</Button>
+			<div className="row justify-content-center d-flex align-items-center">
+				<div className="col-4">
+					<div className="row pt-5 justify-content-center d-flex">
+						<div className="col-12 pb-4">
+							<Input
+								name='username'
+								value={username}
+								placeholder='Enter your username'
+								onChange={(event) => setUsername(event.target.value)}
+							/>
+						</div>
+						<div className="col-12 pb-4">
+							<Input
+								name='username'
+								value={email}
+								placeholder='Enter your email'
+								onChange={(event) => setEmail(event.target.value)}
+							/>
+						</div>
+						<div className="col-12 pb-4">
+							<Button
+								style={{width: '100%'}}
+								type='primary'
+								onClick={handleClick}
+							>Add</Button>
+						</div>
 					</div>
 				</div>
 			</div>
 			<div className="row">
-					<table className='table table-bordered'>
-						<thead>
-							<tr>
-								<th>Username</th>
-								<th>Email</th>
-								<th>Action</th>
-							</tr>
-						</thead>
-						<tbody>
-							{users.map(user => {
-								return (<tr key={user.id}>
-									<td>{user.data().username}</td>
-									<td>{user.data().email}</td>
-									<td><Button type='danger' onClick={() => handleDelete(user.id)}>Remove</Button></td>
-								</tr>)
-							})}
-						</tbody>
-					</table>
+				<table className='table table-bordered'>
+					<thead>
+						<tr>
+							<th>Username</th>
+							<th>Email</th>
+							<th>Action</th>
+						</tr>
+					</thead>
+					<tbody>
+						{users.map(user => {
+							return (<tr key={user.id}>
+								<td>{user.data().username}</td>
+								<td>{user.data().email}</td>
+								<td><Button type='danger' onClick={() => handleDelete(user.id)}>Remove</Button></td>
+							</tr>)
+						})}
+					</tbody>
+				</table>
 
-				</div>
+			</div>
 		</div>
 	);
 }
