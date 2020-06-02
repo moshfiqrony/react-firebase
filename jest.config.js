7be24f8e1e37058d4d17aea05ec5e56e2ca448f0
moshfiqrony <moshfiqrony@gmail.com>
2020-06-02 17:24:56 +0600

    MR | JEST added for the project

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+// // jest.config.js
+// module.exports = {
+//     // ...
+//     watchPlugins: [
+//       [
+//         'path/to/yourWatchPlugin',
+//         {
+//           key: 'k', // <- your custom key
+//           prompt: 'show a custom prompt',
+//         },
+//       ],
+//     ],
+//   };
